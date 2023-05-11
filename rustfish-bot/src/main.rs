e6e3bf710ac774cb49ca2b8a8c1238bd0b91a33a
Bowei Han <bowei.han100@gmail.com>
2023-05-11 16:43:05 -0400

    enumerate UCI commands

--- rustfish-bot/src/main.rs
+++ rustfish-bot/src/main.rs
@@ -1,3 +1,5 @@
+mod uci;
+
 fn main() {
     println!("Hello, world!");
 }
---
+++ rustfish-bot/src/uci.rs
@@ -0,0 +1,62 @@
+pub struct UCIController {}
+
+impl UCIController {}
+
+// UCI Spec: https://www.wbec-ridderkerk.nl/html/UCIProtocol.html
+enum GUICommand {
+    Uci,
+    Debug,
+    IsReady,
+    SetOption,
+    Register,
+    UciNewGame,
+    Position,
+    Go,
+    Stop,
+    PonderHit,
+    Quit,
+}
+
+impl GUICommand {
+    fn value(&self) -> &str {
+        match *self {
+            GUICommand::Uci => "uci",
+            GUICommand::Debug => "debug",
+            GUICommand::IsReady => "isready",
+            GUICommand::SetOption => "setoption",
+            GUICommand::Register => "register",
+            GUICommand::UciNewGame => "ucinewgame",
+            GUICommand::Position => "position",
+            GUICommand::Go => "go",
+            GUICommand::Stop => "stop",
+            GUICommand::PonderHit => "ponderhit",
+            GUICommand::Quit => "quit",
+        }
+    }
+}
+
+enum EngineCommand {
+    Id,
+    UciOk,
+    ReadyOk,
+    BestMove,
+    CopyProtection,
+    Registration,
+    Info,
+    Option,
+}
+
+impl EngineCommand {
+    fn value(&self) -> &str {
+        match *self {
+            EngineCommand::Id => "id",
+            EngineCommand::UciOk => "uciok",
+            EngineCommand::ReadyOk => "readyok",
+            EngineCommand::BestMove => "bestmove",
+            EngineCommand::CopyProtection => "copyprotection",
+            EngineCommand::Registration => "registration",
+            EngineCommand::Info => "info",
+            EngineCommand::Option => "option",
+        }
+    }
+}
